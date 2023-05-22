72df2609f556f43d94aeb2261fcafd2e10523e29
Евеліна Федоренко <ipz192_feo@student.ztu.edu.ua>
2023-05-22 03:13:54 +0300

    react hook installed

---
+++ src/components/form2.js
@@ -0,0 +1,10 @@
+import { useForm } from "react-hook-form";
+import React from "react";
+
+export class Form2 extends React.Component {
+    render() {
+        return(
+            <></>
+        );
+    }
+}
